e75794f57770891dbe9b799a801ebb934812a0fe
JoaoAugustoTopanotti <joaoactopa@gmail.com>
2024-10-01 17:06:08 -0300

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: 'next', // ou 'eslint:recommended', etc.
+    rules: {
+      'no-console': 'warn', // Avisar sobre o uso de console.log
+      'semi': ['error', 'always'], // Exigir ponto e vírgula
+    },
+  };
+  
